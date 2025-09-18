5ad1f4f80f655118a37e926958cab8965b104a9d
bertolucciDev <devluisbertolucci@gmail.com>
2025-09-18 14:57:19 -0300

    chore(lint): add ESLint configuration for React Native with TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,42 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: true,
+    babelOptions: {
+      configFile: './babel.config.js',
+    },
+  },
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'react-native', '@typescript-eslint', 'import', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      babel: {
+        config: './babel.config.js',
+      },
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off', // React 17+ não precisa importar React
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'react-native/no-inline-styles': 'off', // desativa regra chata do RN
+  },
 };
